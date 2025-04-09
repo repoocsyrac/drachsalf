b25a6f48f9f36dfb282d7327fd9e0d5dd2fe725a
Carys <118397197+repoocsyrac@users.noreply.github.com>
2025-04-09 02:07:32 +0100

    Create basic GUI for "homepage"

--- src/main/java/com/mycompany/drachsalf/App.java
+++ src/main/java/com/mycompany/drachsalf/App.java
@@ -1,8 +1,17 @@
 package com.mycompany.drachsalf;
 
+import java.awt.Insets;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,12 +23,33 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = SystemInfo.javaVersion();
-        String javafxVersion = SystemInfo.javafxVersion();
+        BorderPane root = new BorderPane();
+        
+        // menu bar
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        fileMenu.getItems().addAll(new MenuItem("Load"), new MenuItem("Save"));
+        menuBar.getMenus().add(fileMenu);
+        root.setTop(menuBar);
+        
+        // deck list in the center
+        ListView<String> deckListView = new ListView<>();
+        root.setCenter(deckListView);
+        
+        // buttons at the bottom
+        Button createDeckButton = new Button("Create Deck");
+        Button deleteDeckButton = new Button("Delete Deck");
+        Button startReviewButton = new Button("Start Review");
 
-        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(label), 640, 480);
+        HBox buttonBar = new HBox(10, createDeckButton, deleteDeckButton, startReviewButton);
+        buttonBar.setAlignment(Pos.CENTER);
+        buttonBar.setPadding(new javafx.geometry.Insets(10));
+        root.setBottom(buttonBar);
+        
+        // scene
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
+        stage.setTitle("Drachsalf");
         stage.show();
     }
 
