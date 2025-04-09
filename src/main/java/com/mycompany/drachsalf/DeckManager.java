310d464d7fb226877c35d114e8980d6e528b49a1
Carys <118397197+repoocsyrac@users.noreply.github.com>
2025-04-09 19:03:28 +0100

    Add basic functionality to deckmanager; deals with storing all the decks

--- src/main/java/com/mycompany/drachsalf/DeckManager.java
+++ src/main/java/com/mycompany/drachsalf/DeckManager.java
@@ -1,8 +1,35 @@
 
 package com.mycompany.drachsalf;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DeckManager {
-    private List<Deck> decks;
+    private Map<String, Deck> decks;
+    
+    public DeckManager() {
+        this.decks = new HashMap<>();
+    }
+    
+    public void addDeck(Deck deck) {
+        this.decks.put(deck.getName(), deck);
+    }
+
+    public void removeDeck(String name) {
+        this.decks.remove(name);
+    }
+
+    public Deck getDeck(String name) {
+        return this.decks.get(name);
+    }
+
+    public List<String> getDeckNames() {
+        return new ArrayList<>(decks.keySet());
+    }
+    
+    public boolean contains(String name) {
+        return this.decks.containsKey(name);
+    }
 }
