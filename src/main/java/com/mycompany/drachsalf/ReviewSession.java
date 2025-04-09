e8c6ccd30ed6f32d10de5c8b2d41e596992d0578
Carys <118397197+repoocsyrac@users.noreply.github.com>
2025-04-09 01:39:50 +0100

    Add basic functionality to Review Session

--- src/main/java/com/mycompany/drachsalf/ReviewSession.java
+++ src/main/java/com/mycompany/drachsalf/ReviewSession.java
@@ -1,14 +1,35 @@
 
 package com.mycompany.drachsalf;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class ReviewSession {
     private Deck deck;
     private Queue<Card> dueCards;
+    private Card currentCard;
     
     public ReviewSession(Deck deck) {
         this.deck = deck;
-        // this.dueCards = this.deck.getDueCards();
+        this.dueCards = new LinkedList<>(this.deck.getAllDueCards());
+    }
+    
+    public boolean hasNext() {
+        return !this.dueCards.isEmpty();
+    }
+    
+    public Card getCurrentCard() {
+        return this.dueCards.peek();
+    }
+    
+    public void submitReviw(int score) {
+        Card card = this.dueCards.poll(); // Removes card from deck, it has been asnwered.
+        if (card != null) {
+            card.submitReview(score);
+        }
+    }
+    
+    public int getRemainingCount() {
+        return this.dueCards.size();
     }
 }
