a7d5e524cf91b011a26c40fafa505164fcb3266b
Carys <118397197+repoocsyrac@users.noreply.github.com>
2025-04-09 01:20:28 +0100

    Add basic functionality to deck: add card, remove card, get cards, get name

--- src/main/java/com/mycompany/drachsalf/Deck.java
+++ src/main/java/com/mycompany/drachsalf/Deck.java
@@ -12,4 +12,41 @@ public class Deck {
         this.name = name;
         this.cards = new ArrayList<>();
     }
+    
+    // TODO: overload constructor to create Deck directly from file
+    /*
+    public Deck(Path file) throws IOException {
+        // load deck data from file
+    }
+    */
+    
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+    
+    public void removeCard(Card card) {
+        this.cards.remove(card);
+    }
+    
+    public List<Card> getAllCards() {
+        return this.cards;
+    }
+    
+    public List<Card> getAllDueCards() {
+        List<Card> dueCards = new ArrayList<>();
+        
+        for(Card card : this.cards) {
+            if(card.isDue()) {
+                dueCards.add(card);
+            }
+        }
+        
+        return dueCards;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    //TODO: save to file, load from file
 }
